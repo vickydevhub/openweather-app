24053bd4f9beb5a2c93c584008db3fdd53fedc0a
Vivek Kumar <vivek@fluidlabs.co.uk>
2023-07-12 18:01:01 +0530

    update git actions

--- tests/Feature/CityTest.php
+++ tests/Feature/CityTest.php
@@ -6,12 +6,3 @@ it('has city page', function () {
     $response->assertStatus(200);
 });
 
-it('can fetch a weather', function () {
-     
-     
-    // Make a request to the API for each city and check the response
-     
-    $response = $this->getJson('/api/v1/weather');
- 
-    $response->assertStatus(200)->assertJson($response);
-});
