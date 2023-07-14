9a0ff44b4d1da87e0cac0a2505d02c08dbb53b66
Vivek Kumar <vivek@fluidlabs.co.uk>
2023-07-14 10:05:56 +0530

    update CI file and create new test for city name valdiation

--- tests/Feature/CityTest.php
+++ tests/Feature/CityTest.php
@@ -1,8 +1,20 @@
 <?php
 use App\Models\City;
+use Illuminate\Http\Response;
+use function Pest\Laravel\post;
+
 it('has city page', function () {
     $response = $this->get('/cities');
 
     $response->assertStatus(200);
 });
 
+it('should return 422 if name is missing', function ($name) {
+    post(route('cities.store'), [
+        'name' => $name,
+    ])->assertStatus(302)->assertSessionHasErrors(['name']);
+})->with([
+    '', 
+]);
+
+ 
